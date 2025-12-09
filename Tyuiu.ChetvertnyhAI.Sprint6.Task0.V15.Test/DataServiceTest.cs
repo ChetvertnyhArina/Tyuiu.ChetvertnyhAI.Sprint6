db0168617b0dca3eb9fc0f4abad8ff29e5adc1f1
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-12-09 21:33:18 +0500

    Add project files.

---
+++ Tyuiu.ChetvertnyhAI.Sprint6.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ChetvertnyhAI.Sprint6.Task0.V15.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint6.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 64;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
